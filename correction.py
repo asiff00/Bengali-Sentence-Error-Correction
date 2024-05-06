f22acdff5551d3b4a88101701f3dd383ac0b4d89
himisir <31031926+himisir@users.noreply.github.com>
2024-05-06 19:19:09 +0600

    Update correction.py

--- correction.py
+++ correction.py
@@ -14,7 +14,6 @@ def model_call(checkpoint):
         checkpoint, src_lang="bn_IN", tgt_lang="bn_IN", use_fast=True
     )
     model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint, use_safetensors=True)
-    model = model.to(device)
     return tokenizer, model
 
 
@@ -34,21 +33,20 @@ def chat_loop(tokenizer, model):  # Chat loop! enter 'quit' to quit
             inputs,
             max_new_tokens=len(given_sentence),
             early_stopping=True,
-        )
+        ).to(device)
         print(
             f"Correct sentence: {tokenizer.decode(output_ids[0], skip_special_tokens=True)}"
         )
 
 
 try:
-    c = 1 / 0  # Intentional, currently there's an error, so it may not work.
-    checkpoint = "asif00/mbart_bn_error_correction"  # Currently there's an error, so it may not work.
+    checkpoint = "asif00/mbart_bn_error_correction" 
     tokenizer, model = model_call(checkpoint)
     chat_loop(tokenizer, model)
 
 except (
     Exception
-) as e:  # Backup files from my google drive, 2GB download, may take a few minutes
+) as e:  # Backup files from my Google Drive, 2GB download, may take a few minutes
     checkpoint = "checkpoint"
     download_file_from_google_drive(id, dir, file)
     tokenizer, model = model_call(checkpoint)
